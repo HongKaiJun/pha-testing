542a7f4fec82e2a1b6261ea0b081269f3d11b954
HongKaiJun <kaijun2110@gmail.com>
2021-10-18 11:06:54 +0800

    ui design for login page

--- src/components/Userlogin.jsx
+++ src/components/Userlogin.jsx
@@ -15,11 +15,16 @@ class Userlogin extends Component {
         return (
             <div className = 'login_box'>
                 <form className ="company_logo">
-                    
+                        {/*<img src= {require("./images/employeelogo.png")} alt="Employee_Logo"></img></div>*/}
+                        <br/>
+                        <button style={{fontSize:"16px",width:"320px", height:"100px"}} className = "btn btn-danger">SYSTEM USER</button>
+                        <button style={{fontSize:"16px",width:"320px", height:"100px"}} className = "btn btn-danger">USER</button>
 
-                        <button className = "btn btn-danger">USER</button>
-                        <button className = "btn btn-danger">SYSTEM USER</button>
+                        <input style={{borderRadius: "10px", marginTop: "20px",width:"500px", height:"50px"}} placeholder="Username" name = "UserName" className="form-control"/>
 
+                        <input  style={{borderRadius: "10px", marginTop: "20px",width:"500px", height:"50px"}} placeholder="Password" name = "UserName" className="form-control"/>
+                        <br/>
+                        <button  style={{marginRight:"400px",borderRadius: "10px", marginTop: "40px",marginBottom: "20px",width:"100px", height:"50px"}} className = "btn btn-danger">LOGIN</button>
         
  
                 </form>
