79e32d134c5732d636fc9bf25f26292c8bb956a3
HongKaiJun <kaijun2110@gmail.com>
2021-10-26 16:51:23 +0800

    login condition

--- src/components/Userlogin.jsx
+++ src/components/Userlogin.jsx
@@ -9,11 +9,35 @@ class Userlogin extends Component {
         this.state = {
             userName: '',
             password:'',
+            isSystem: false
         }
-
+       
+        
     }
 
     // ComponenetDidMount to fetch the employeedata from the backend to check for login purpose
+    componentDidMount(){
+
+        if(this.state.isSystem === false){
+            // Display the Azure Login Page
+
+        }else{
+            //Display the Manual Login Page 
+
+        }
+    }
+
+    setUser=(e)=>{
+        e.preventDefault();
+        this.setState({isSystem: false})
+        console.log(this.state.isSystem);
+    }
+
+    setSystemUser=(e)=>{
+        e.preventDefault();
+        this.setState({isSystem: true})
+        console.log(this.state.isSystem);
+    }
 
     Login =(e)=>{
         e.preventDefault();
@@ -36,19 +60,21 @@ class Userlogin extends Component {
                         {/*<img src= {require("./images/employeelogo.png")} alt="Employee_Logo"></img></div>*/}
                         <img src= {LOGO1} style={{width:"85%", height:"60px",marginBottom:"20px"}} alt="Employee_Logo"></img>
                         <br/>
-                        <button style={{fontSize:"16px",width:"320px", height:"90px"}} className = "User_Button">SYSTEM USER</button>
-                        <button style={{fontSize:"16px",width:"320px", height:"90px"}} className = "System_User_Button">USER</button>
+                        <button style={{fontSize:"16px",width:"320px", height:"90px"}} className = "System_User_Button" onClick={this.setUser}>USER</button>
+                        <button style={{fontSize:"16px",width:"320px", height:"90px"}} className = "User_Button" onClick={this.setSystemUser}>SYSTEM USER </button>
+                        
 
-                        <input style={{fontSize:"16px",borderRadius: "10px", marginTop: "20px",width:"500px", height:"50px",border: "2px solid rgb(207, 200, 200)"}} 
+                        <input style={{fontSize:"16px",borderRadius: "10px", marginTop: "20px",width:"500px", height:"50px",border: "2px solid rgb(207, 200, 200)", display:'none'}} 
                         placeholder="  Username" name = "UserName" className="input_username" value ={this.state.userName} onChange={this.changeUserNameHandler}/>
 
-                        <input  style={{fontSize:"16px",borderRadius: "10px", marginTop: "20px",width:"500px", height:"50px",border: "2px solid rgb(207, 200, 200)"}} 
+                        <input  style={{fontSize:"16px",borderRadius: "10px", marginTop: "20px",width:"500px", height:"50px",border: "2px solid rgb(207, 200, 200)", display:'none'}} 
                         placeholder="  Password" name = "UserName" className="input_password" value ={this.state.password} onChange={this.changePasswordHandler}/>
                         <br/>
-                        <button  style={{fontSize:"16px",marginRight:"400px",borderRadius: "10px", marginTop: "40px",marginBottom: "20px",width:"100px", height:"50px"}} 
+                        <button  style={{fontSize:"16px",marginRight:"400px",borderRadius: "10px", marginTop: "40px",marginBottom: "20px",width:"100px", height:"50px", display:'none'}} 
                         onClick = {this.Login} className = "Login_Button">LOGIN</button>
         
- 
+                        <button  style={{fontSize:"16px",borderRadius: "10px", marginTop: "40px",marginBottom: "50px",width:"100px", height:"50px"}} 
+                                         className = "Login_Button">LOGIN</button>
                 </form>
 
             </div>
