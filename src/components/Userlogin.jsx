60c1ce2de93f6c0c4e717bdcf4aef50efe19134c
HongKaiJun <kaijun2110@gmail.com>
2021-10-27 09:42:47 +0800

    Seperate login form with condition rerender

--- src/components/Userlogin.jsx
+++ src/components/Userlogin.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import LOGO1 from "../images/employeelogo.png";
+import SystemUserlogin from './SystemUserlogin';
 
 
 class Userlogin extends Component {
@@ -7,25 +8,26 @@ class Userlogin extends Component {
         super(props)
 
         this.state = {
-            userName: '',
-            password:'',
-            isSystem: false
+            isSystem: false,
+           
         }
        
         
     }
 
     // ComponenetDidMount to fetch the employeedata from the backend to check for login purpose
-    componentDidMount(){
+   // componentDidMount(){
 
-        if(this.state.isSystem === false){
+        //if(!this.state.isSystem){
             // Display the Azure Login Page
+            //console.log("Only Login button");
+            
 
-        }else{
+       // }else{
             //Display the Manual Login Page 
-
-        }
-    }
+            //console.log("Display textfield with button");
+       // }
+   // }
 
     setUser=(e)=>{
         e.preventDefault();
@@ -39,42 +41,27 @@ class Userlogin extends Component {
         console.log(this.state.isSystem);
     }
 
-    Login =(e)=>{
-        e.preventDefault();
-        console.log("Username:"+this.state.userName+ "Password:" +this.state.password );
-    }
-    
-
-    changeUserNameHandler =(event)=>{
-        this.setState({userName:event.target.value});
-    }
-
-    changePasswordHandler =(event)=>{
-        this.setState({password:event.target.value});
-    }
-
     render() {
+
+        let {isSystem}  = this.state;
+
         return (
             <div className = 'login_box'>
                 <form className ="main_page">
                         {/*<img src= {require("./images/employeelogo.png")} alt="Employee_Logo"></img></div>*/}
                         <img src= {LOGO1} style={{width:"85%", height:"60px",marginBottom:"20px"}} alt="Employee_Logo"></img>
                         <br/>
-                        <button style={{fontSize:"16px",width:"320px", height:"90px"}} className = "System_User_Button" onClick={this.setUser}>USER</button>
-                        <button style={{fontSize:"16px",width:"320px", height:"90px"}} className = "User_Button" onClick={this.setSystemUser}>SYSTEM USER </button>
                         
-
-                        <input style={{fontSize:"16px",borderRadius: "10px", marginTop: "20px",width:"500px", height:"50px",border: "2px solid rgb(207, 200, 200)", display:'none'}} 
-                        placeholder="  Username" name = "UserName" className="input_username" value ={this.state.userName} onChange={this.changeUserNameHandler}/>
-
-                        <input  style={{fontSize:"16px",borderRadius: "10px", marginTop: "20px",width:"500px", height:"50px",border: "2px solid rgb(207, 200, 200)", display:'none'}} 
-                        placeholder="  Password" name = "UserName" className="input_password" value ={this.state.password} onChange={this.changePasswordHandler}/>
-                        <br/>
-                        <button  style={{fontSize:"16px",marginRight:"400px",borderRadius: "10px", marginTop: "40px",marginBottom: "20px",width:"100px", height:"50px", display:'none'}} 
-                        onClick = {this.Login} className = "Login_Button">LOGIN</button>
-        
+                        <button style={{fontSize:"16px",width:"320px", height:"90px"}} className = "User_Button" onClick={this.setUser}>USER</button>
+                        <button style={{fontSize:"16px",width:"320px", height:"90px"}} className = "System_User_Button" onClick={this.setSystemUser}>SYSTEM USER </button>
+                        
+                        {/* conditional rerendering for different login display form*/}
+                        {isSystem?
+                        <SystemUserlogin/>:
                         <button  style={{fontSize:"16px",borderRadius: "10px", marginTop: "40px",marginBottom: "50px",width:"100px", height:"50px"}} 
-                                         className = "Login_Button">LOGIN</button>
+                                         className = "Login_Button">LOGIN</button>}
+
+                        
                 </form>
 
             </div>
